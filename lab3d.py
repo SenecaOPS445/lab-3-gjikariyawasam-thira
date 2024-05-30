6dfec07130b3daca6cd744440b8ec89dc9d1ab76
Jihan.Kariyawasam <gjikariyawasam-thira@myseneca.ca>
2024-05-30 19:25:57 -0400

    Added lab3d

--- lab3d.py
+++ lab3d.py
@@ -7,11 +7,14 @@ import subprocess
 
 def free_space():
     free_space = subprocess.Popen("df -h | grep '/$' | awk '{print $4}'", stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    output = free_space.communicate() 
+    
+    output = free_space.communicate()
+    
     stdout = output[0].decode('utf-8').strip()
+    
     return stdout
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     print(free_space())
 
 
